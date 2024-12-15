4191b3f71f7cf2719785548849fc2f403348a379
Santhosh <71515865+SanthoshKumar9028@users.noreply.github.com>
2024-12-15 18:55:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,121 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+require("dotenv").config();
+const URL = require("url").URL;
+let express = require("express");
+const cors = require("cors");
+let path = require("path");
+let bodyParser = require("body-parser");
+const { default: mongoose } = require("mongoose");
 
-app.use(cors())
-app.use(express.static('public'))
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+let app = express();
+
+app.use((req, res, next) => {
+  console.log(`\n${req.method}-------`);
+  next();
 });
 
+app.use(cors());
 
+app.use("/public", express.static(path.join(__dirname, "public")));
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+mongoose.connect(process.env.MONGO_URI);
+
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+});
+
+const LogSchema = new mongoose.Schema({
+  description: { type: String, required: true },
+  duration: { type: Number, required: true },
+  date: { type: Date, default: Date.now },
+  userId: [{ type: mongoose.Schema.ObjectId, ref: "User" }],
+});
+
+const User = mongoose.model("User", UserSchema);
+const Log = mongoose.model("Log", LogSchema);
+
+app.get("/api/clear", async (req, res) => {
+  await Log.deleteMany();
+  await User.deleteMany();
+  res.send("Cleared");
+});
+
+app.post("/api/users", async (req, res) => {
+  const user = new User({
+    username: req.body.username,
+  });
+
+  await user.save();
+
+  res.json(user);
+});
+
+app.get("/api/users", async (req, res) => {
+  const users = await User.find();
+
+  res.json(users);
+});
+
+app.post("/api/users/:_id/exercises", async (req, res) => {
+  const user = await User.findById(req.params._id);
+
+  const newLog = new Log({
+    description: req.body.description,
+    date: req.body.date,
+    duration: req.body.duration,
+    userId: user._id,
+  });
+
+  await newLog.save();
+
+  const resObj = {
+    _id: user._id,
+    username: user.username,
+    description: newLog.description,
+    duration: newLog.duration,
+    date: newLog.date.toDateString(),
+  };
+
+  res.json(resObj);
+});
+
+app.get("/api/users/:_id/logs", async (req, res) => {
+  const user = await User.findById(req.params._id);
+  const logsQuery = Log.find({
+    userId: user._id,
+  });
+
+  if (req.query.from) {
+    logsQuery.gte("date", new Date(req.query.from));
+  }
+  if (req.query.to) {
+    logsQuery.lte("date", new Date(req.query.to));
+  }
+  if (+req.query.limit) {
+    logsQuery.limit(+req.query.limit);
+  }
+
+  const logs = await logsQuery.exec();
+
+  const resObj = {
+    username: user.username,
+    _id: user._id,
+    count: logs.length,
+    log: logs?.map((log) => ({
+      description: log.description,
+      duration: log.duration,
+      date: log.date.toDateString(),
+    })),
+  };
+
+  console.log(resObj);
+  res.json(resObj);
+});
 
 
 const listener = app.listen(process.env.PORT || 3000, () => {
